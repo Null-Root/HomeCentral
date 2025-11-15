eca037d5ee571108f7690c34416d82215b2904dc
Null-Root <98098486+Null-Root@users.noreply.github.com>
2025-11-16 01:09:12 +0800

    Add API Key, since this is firebase, security is in firestore rules, not here

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDvE-RKXm6MTYI6UnbKn8vBKfMIMhRVni4",
+    authDomain: "home-central-845a2.firebaseapp.com",
+    projectId: "home-central-845a2",
+    storageBucket: "home-central-845a2.firebasestorage.app",
+    messagingSenderId: "868569404985",
+    appId: "1:868569404985:web:e07186d79c6f2ec8ae9fe8",
+    measurementId: "G-M1CVFB19KP"
+
+};
