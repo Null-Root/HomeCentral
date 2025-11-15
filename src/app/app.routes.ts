9e77e6dcede76d8049f716409f4baa623eb6e0f2
Null-Root <98098486+Null-Root@users.noreply.github.com>
2025-11-16 01:10:01 +0800

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		loadComponent: () => import('./home/home.component').then((m) => m.HomeComponent)
+	},
+	{
+		path: 'trackings',
+		loadComponent: () => import('./trackings/trackings.component').then((m) => m.TrackingsComponent)
+	},
+	{ path: '**', redirectTo: '' }
+];
